baa23c022623d301c5ad5a5ca69896c80b302ad4
Alex White <alex_p_white@yahoo.co.uk>
2025-01-16 21:53:15 +0000

    app for listening to all server side events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, SubscriptionLike, tap } from 'rxjs';
+import { EventSourceService, Item, Message } from './event-source.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'serversideevents-ui';
+
+  private readonly eventSourceSubscription: SubscriptionLike;
+
+  warnings$ = new BehaviorSubject<Item[]>([
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+  ]);
+  items$ = new BehaviorSubject<Item[]>([
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+    {
+      Name: 'Name',
+      Value: 0
+    },
+  ]);
+
+  constructor(private eventSourceService: EventSourceService) {
+    const url = 'http://localhost:5225/events';
+    const eventNames = ['Information','Warning'];
+    this.eventSourceSubscription = this.eventSourceService.connectToServerSentEvents(url, eventNames).pipe(
+      tap( (data: Message) => {
+        if(data.type == 'information') {
+          const current = this.items$.value;
+          current.push(data.item);
+          if(current.length>10) {
+            current.shift();
+          }
+          this.items$.next(current);
+          return;
+        }
+        if(data.type == 'warning') {
+          const current = this.warnings$.value;
+          current.push(data.item);
+          if(current.length>10) {
+            current.shift();
+          }
+          this.warnings$.next(current);
+        }
+      })
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.eventSourceSubscription?.unsubscribe();
+    this.eventSourceService.close();
+}
+
 }
---
+++ src/app/event-source.service.ts
@@ -0,0 +1,84 @@
+import { Injectable, NgZone } from "@angular/core";
+import { Observable, Subscriber } from "rxjs";
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+
+export interface Message {
+    type: string;
+    item: Item;
+}
+export interface Item {
+  Name: string,
+  Value: number
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EventSourceService {
+    private eventSource?: EventSource | null;
+
+    constructor(private zone: NgZone) {}
+
+    connectToServerSentEvents(url: string, eventNames: string[] = []): Observable<Message> {
+        // this.eventSource = this.getEventSource(url, options);
+        const self = this;
+        return new Observable((subscriber: Subscriber<Message>) => {
+            fetchEventSource(url, {
+                headers: {
+                    Authorization: 'Bearer sdkfnuiskdjhafgeiut8435u93024uyqr237alweihdfsj'
+                },
+                onerror(error) {
+                    self.zone.run(() => subscriber.error(error));
+                },
+                onmessage(msg) {
+                    if(eventNames.findIndex(n => n === msg.event) > -1) {
+                        console.log(`New ${msg.event} received`);
+                        const item: Item = JSON.parse(msg.data);
+                        self.zone.run(() => subscriber.next({
+                            type: msg.event.toLowerCase(),
+                            item
+                        }));
+                    }
+                },
+            })});
+ 
+        // return new Observable((subscriber: Subscriber<Message>) => {
+        //     if(this.eventSource != null) {
+        //         this.eventSource.onerror = error => {
+        //             this.zone.run(() => subscriber.error(error));
+        //         };
+        //         this.eventSource.addEventListener('Information', (event) => {
+        //             console.log("New information received");
+        //             const item: Item = JSON.parse(event.data);
+        //             this.zone.run(() => subscriber.next({
+        //                 type: "information",
+        //                 item
+        //             }));
+        //         })
+        //         this.eventSource.addEventListener('Warning', (event) => {
+        //             console.log("New warning received");
+        //             const item: Item = JSON.parse(event.data);
+        //             this.zone.run(() => subscriber.next({
+        //                 type: "warning",
+        //                 item
+        //             }));
+        //         })
+        //         // captures everything
+        //         // this.eventSource.onmessage = (event) => {
+        //         //     const item: Item = JSON.parse(event.data);
+        //         //     console.log("New item received:", JSON.stringify(event));
+        //         //     this.zone.run(() => subscriber.next(item));
+        //         // };
+        //     }
+        // });
+    }
+
+    close(): void {
+        if (!this.eventSource) {
+            return;
+        }
+ 
+        this.eventSource.close();
+        this.eventSource = null;
+    }
+}
